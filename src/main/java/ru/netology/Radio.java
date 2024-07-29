3c9be10b1566a1909ef6923a29fa783d3fef511f
loach99 <constantinent@mail.ru>
2024-07-29 15:04:51 +0300

    first commit

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,67 @@
+package ru.netology;
+
+public class Radio {
+    // громкость звука
+    private int currentVolume;
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setIncreaseVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolumeNext() {
+        if (currentVolume < 100) {
+            currentVolume++;
+            return;
+        }
+        currentVolume = 100;
+    }
+
+    public void increaseVolumePrev() {
+        if (currentVolume > 0) {
+            currentVolume--;
+            return;
+        }
+        currentVolume = 0;
+    }
+
+    // Радио станция
+    private int stationRadio;
+
+    public int getStationRadio() {
+        return stationRadio;
+    }
+
+    public void setStationRadio(int stationRadio) {
+        if (stationRadio > 9) {
+            return;
+        }
+        if (stationRadio < 0) {
+            return;
+        }
+        this.stationRadio = stationRadio;
+    }
+
+    public void stationRadioNext() {
+        if (stationRadio != 9) {
+            stationRadio++;
+            return;
+        }
+        stationRadio = 0;
+    }
+
+    public void stationRadioPrev() {
+        if (stationRadio != 0) {
+            stationRadio--;
+            return;
+        }
+        stationRadio = 9;
+    }
+}
+
+
+
+
---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,190 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+    @Test
+    public void StationRadioNext9() {
+        Radio service = new Radio();
+
+        service.setStationRadio(9);
+        service.stationRadioNext();
+
+        int expected = 0;
+        int actual = service.getStationRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationRadioNext0() {
+        Radio service = new Radio();
+
+        service.setStationRadio(0);
+        service.stationRadioNext();
+
+        int expected = 1;
+        int actual = service.getStationRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationRadioNext8() {
+        Radio service = new Radio();
+
+        service.setStationRadio(8);
+        service.stationRadioNext();
+
+        int expected = 9;
+        int actual = service.getStationRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationRadio1() {
+        Radio service = new Radio();
+
+        service.setStationRadio(1);
+
+        int expected = 1;
+        int actual = service.getStationRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationRadio11() {
+        Radio service = new Radio();
+
+        service.setStationRadio(11);
+
+        int expected = 0;
+        int actual = service.getStationRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationRadio5() {
+        Radio service = new Radio();
+
+        service.setStationRadio(-5);
+
+        int expected = 0;
+        int actual = service.getStationRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationRadio9() {
+        Radio service = new Radio();
+
+        service.setStationRadio(9);
+
+        int expected = 9;
+        int actual = service.getStationRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationRadioPrev0() {
+        Radio service = new Radio();
+
+        service.setStationRadio(0);
+        service.stationRadioPrev();
+
+        int expected = 9;
+        int actual = service.getStationRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationRadioPrev9() {
+        Radio service = new Radio();
+
+        service.setStationRadio(9);
+        service.stationRadioPrev();
+
+        int expected = 8;
+        int actual = service.getStationRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void volumeRadioNext0() {
+        Radio service = new Radio();
+        service.setIncreaseVolume(0);
+        service.increaseVolumeNext();
+
+        int expected = 1;
+        int actual = service.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void volumeRadioNext100() {
+        Radio service = new Radio();
+        service.setIncreaseVolume(100);
+        service.increaseVolumeNext();
+
+        int expected = 100;
+        int actual = service.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void volumeRadioNext1() {
+        Radio service = new Radio();
+        service.setIncreaseVolume(-1);
+        service.increaseVolumeNext();
+
+        int expected = 0;
+        int actual = service.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void volumeRadioNext101() {
+        Radio service = new Radio();
+        service.setIncreaseVolume(101);
+        service.increaseVolumeNext();
+
+        int expected = 100;
+        int actual = service.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void volumeRadioPrev0() {
+        Radio service = new Radio();
+        service.setIncreaseVolume(0);
+        service.increaseVolumePrev();
+
+        int expected = 0;
+        int actual = service.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void volumeRadioPrev100(){
+        Radio service = new Radio();
+        service.setIncreaseVolume(100);
+        service.increaseVolumePrev();
+
+        int expected = 99;
+        int actual = service.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
